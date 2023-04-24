d5d76315b733446efad42f6809861d7173da0bb0
Nick <66189083+urnotnick@users.noreply.github.com>
2023-04-24 10:14:42 -0500

    Compute Right Vector for VM roll

--- ViewModel/ViewModel.gd
+++ ViewModel/ViewModel.gd
@@ -22,6 +22,7 @@ func walk_bob(md: float):
 func _input(event):
 	if (event is InputEventMouseMotion):
 		AccelerationX += deg_to_rad(event.relative.x * .06);
+		AccelerationY += deg_to_rad(event.relative.y * .06);
 		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,12 +30,16 @@ func _process(delta: float):
 	curtime += delta;
 	
 	AccelerationX = lerpf(AccelerationX, 0, delta * 7.5);
+	AccelerationY = lerpf(AccelerationY, 0, delta * 7.5);
 	
-	var rightVector: Vector3 = Player.rotation;
-	rightVector.rotated(Vector3(0, 1, 0), 90);
-	var rd: float = Player.velocity.normalized().dot(rightVector);
+	var rightVector: Vector3 = Vector3(
+		cos(Player.rotation.y),
+		0,
+		-sin(Camera.rotation.x)
+	);
+	var rd: float = rightVector.dot(Player.velocity.limit_length(20) / 20);
 	var fd: float = Player.velocity.limit_length(1).length() * .01;
 	
 	
-	rotation = Camera.rotation + Vector3(0, (AccelerationX * -.28) - (rd * .1), AccelerationX - (rd / 2));
+	rotation = Camera.rotation + Vector3(AccelerationY * .2, (AccelerationX * -.28) - (rd * .02), AccelerationX - (rd * 1));
 	position = self.walk_bob(fd) + Vector3(AccelerationX * .02 + .07, 1.4, -.002);
