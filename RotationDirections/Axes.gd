8ffed64c1e7099988130661df7fb526b2e452a0b
Nick <66189083+urnotnick@users.noreply.github.com>
2023-04-24 11:42:09 -0500

    Add AutoLoad Axes thing

---
+++ RotationDirections/Axes.gd
@@ -0,0 +1,28 @@
+extends Node
+
+func forward(node: Node3D) -> Vector3:
+	var rot := node.rotation as Vector3;
+	var pitch: float = rot.x;
+	var yaw: float = rot.y;
+	var roll: float = rot.z;
+	var v: Vector3 = Vector3(
+		cos(pitch) * sin(yaw),
+		-sin(pitch),
+		cos(pitch) * cos(yaw)
+	);
+	return v;
+	
+func right(node: Node3D) -> Vector3:
+	var rot := node.rotation as Vector3;
+	var pitch: float = rot.x;
+	var yaw: float = rot.y;
+	var roll: float = rot.z;
+	var v: Vector3 = Vector3(
+		cos(yaw),
+		0,
+		-sin(yaw)
+	);
+	return v;
+
+func up(node: Node3D) -> Vector3:
+	return forward(node).cross(right(node));
