88d1c4bf4ff8ead5da341da361c788b78d1b13ff
Nick <66189083+urnotnick@users.noreply.github.com>
2023-04-28 07:50:27 -0500

    Added copyright notices, removed useless classes

--- CombatNode3D/Combat3D.gd
+++ CombatNode3D/Combat3D.gd
@@ -1,9 +1,14 @@
 # Copyright (c) 2023 - Nick S. - All Rights Reserved.
 # ===================================================
-# This file was shared publicly, please do not remove
-# this credit part.
+# This file was shared publicly, please do not remove credit part.
 # https://github.com/urnotnick/godot-stuff
 # ===================================================
+
+# If you use this code in any project, please put this somewhere in scripts where it is used:
+# [DESCRIPTION OF CODE USED, FUNCTION/PURPOSE]
+# Copyright (c) 2023 - Nick S.
+# Source: https://github.com/urnotnick/godot-stuff (Link to specific file if applicable)
+
 class_name Combat3D
 extends Node3D
 
--- Extensions/NodeExtension.gd
+++ Extensions/NodeExtension.gd
@@ -1,10 +1,15 @@
 # Copyright (c) 2023 - Nick S. - All Rights Reserved.
 # ===================================================
-# This file was shared publicly, please do not remove
-# this credit part.
+# This file was shared publicly, please do not remove credit part.
 # https://github.com/urnotnick/godot-stuff
 # ===================================================
+
+# If you use this code in any project, please put this somewhere in scripts where it is used:
+# [DESCRIPTION OF CODE USED, FUNCTION/PURPOSE]
+# Copyright (c) 2023 - Nick S.
+# Source: https://github.com/urnotnick/godot-stuff (Link to specific file if applicable)
+
 extends Node;
 func assert_instance(node_instance: Node) -> Node:
-	assert(node_instance != null, "Assertion of NULL instance.");
+	assert(node_instance, "Assertion of NULL instance.");
 	return node_instance;
